b78614e6335a4d23ac6301cd19bf3be8536b24fc
mitnick <1102743539@qq.com>
2017-07-18 16:52:53 +0800

    Create ProgressListener.java

---
+++ app/ProgressListener.java
@@ -0,0 +1,14 @@
+package com.zte.fn.iop.zparking.modules.service;
+
+/**
+ * Created by 0048104325 on 2017/7/18.
+ */
+
+public interface ProgressListener {
+    /**
+     * @param progress     已经下载或上传字节数
+     * @param total        总字节数
+     * @param done         是否完成
+     */
+    void onProgress(long progress, long total, boolean done);
+}
