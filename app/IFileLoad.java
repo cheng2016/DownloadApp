719dfae7c05be397973899921bea5a311a299abf
mitnick <1102743539@qq.com>
2017-07-18 16:52:34 +0800

    Create IFileLoad.java

---
+++ app/IFileLoad.java
@@ -0,0 +1,14 @@
+package com.zte.fn.iop.zparking.modules.service;
+
+import io.reactivex.Observable;
+import okhttp3.ResponseBody;
+import retrofit2.http.GET;
+
+/**
+ * Created by 0048104325 on 2017/7/18.
+ */
+
+public interface IFileLoad {
+    @GET("iop-zparking-0.0.1.20170718092339.apk")
+    Observable<ResponseBody> loadFile();
+}
