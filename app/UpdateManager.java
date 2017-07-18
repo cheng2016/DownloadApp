df658fc40808d60b34223bd2d3c61d62e2841461
mitnick <1102743539@qq.com>
2017-07-18 16:53:35 +0800

    Create UpdateManager.java

---
+++ app/UpdateManager.java
@@ -0,0 +1,84 @@
+package com.zte.fn.iop.zparking.modules.service;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.zte.fn.iop.zparking.util.DeviceUtils;
+
+/**
+ * Created by zs on 2016/7/7.
+ */
+public class UpdateManager {
+    private static UpdateManager instance;
+
+    public static UpdateManager getInstance(Context context){
+        if(instance == null){
+            synchronized (UpdateManager.class){
+                instance = new UpdateManager(context);
+            }
+        }
+        return instance;
+    }
+
+    private Context mContext;
+
+    public UpdateManager(Context context) {
+        this.mContext = context;
+    }
+
+    /**
+     * 检测软件更新
+     */
+    public boolean checkUpdate(final boolean isToast) {
+        /**
+         * 在这里请求后台接口，获取更新的内容和最新的版本号
+         */
+        // 版本的更新信息
+        String version_info = "更新内容\n" + "    1. 车位分享异常处理\n" + "    2. 发布车位折扣格式统一\n" + "    ";
+        int mVersion_code = DeviceUtils.getVersionCode(mContext);// 当前的版本号
+        int nVersion_code = 2;
+        if (mVersion_code < nVersion_code) {
+            // 显示提示对话
+//            showNoticeDialog(version_info);
+            return true;
+        } else {
+            if (isToast) {
+                Toast.makeText(mContext, "已经是最新版本", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 显示更新对话框
+     *
+     * @param version_info
+     */
+    private void showNoticeDialog(String version_info) {
+        // 构造对话框
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("更新提示");
+        builder.setMessage(version_info);
+        // 更新
+        builder.setPositiveButton("立即更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                mContext.startService(new Intent(mContext, DownloadService.class));
+            }
+        });
+        // 稍后更新
+        builder.setNegativeButton("以后更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        Dialog noticeDialog = builder.create();
+        noticeDialog.show();
+    }
+}
